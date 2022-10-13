519338b536dec8f608e45f48ac8c26bd996432ed
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2022-10-13 18:19:29 -0400

    Started working on gizmos to render grid

--- WFC Project/Assets/Scripts/WFCGenerator.cs
+++ WFC Project/Assets/Scripts/WFCGenerator.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 
 public class WFCGenerator : MonoBehaviour
 {
     [Space , Header("Generator Settings")]
     public GameObject cellStartingPos;
-    public int gridSizeX, gridSizeZ;
+    public int gridWidth, gridLength;
     public float cellSizeX, cellSizeZ;
 
+    [Space, Header("Debug Settings")]
+    public bool toggleGizmos = false;
+    public bool toggleDebugInfo = false;
 
     private gridCell[,] gridArray;
 
@@ -28,12 +31,12 @@ public class WFCGenerator : MonoBehaviour
     void regenerateGrid()
     {
         //Remake Array WIth Desired Size
-        gridArray = new gridCell[gridSizeX, gridSizeZ];
+        gridArray = new gridCell[gridWidth, gridLength];
 
         //iterates through the entier grid and initializes the struct at that spesific ID, with all the required information
-        for (int x = 0; x < gridSizeX; x++)
+        for (int x = 0; x < gridWidth; x++)
         {
-            for (int z = 0; z < gridSizeZ; z++)
+            for (int z = 0; z < gridLength; z++)
             {
                 //temp variables to do basic calculation
                 Vector2Int tempId = new Vector2Int(x, z);
@@ -47,6 +50,7 @@ public class WFCGenerator : MonoBehaviour
 
     void printGridInfo()
     {
+        if (toggleDebugInfo == false) return;
         //iterates through the entier grid 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
@@ -60,12 +64,43 @@ public class WFCGenerator : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        //to stop errors from happening this just allows me to turn on and off gizmos at will
+        if (toggleGizmos == false) return;
+
+
+        //itterates through each cell and places a lable explaining its Cell ID
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
             for (int z = 0; z < gridArray.GetLength(1); z++)
             {
-                
+                Handles.color = Color.blue;
+
+                Vector3 tempHandlePos = new Vector3(gridArray[x, z].cellPos.x - (cellSizeX / 2), 0, gridArray[x, z].cellPos.z - (cellSizeZ / 2));
+                Handles.Label(tempHandlePos, gridArray[x, z].cellId.ToString());
             }
+        }
+
+        //Generates top and left lines
+        float widthLineEndPosX = cellStartingPos.transform.position.x * gridWidth;
+        Vector3 widthLineEndPos = new Vector3(widthLineEndPosX, 0, cellStartingPos.transform.position.z);
+
+        Gizmos.DrawLine(cellStartingPos.transform.position, widthLineEndPos);
+
+        //itterates through each horizontal grid cell to place a line down
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            Gizmos.color = Color.white;
+
+            float tempStartPosX = gridArray[x, 0].cellPos.x + (cellSizeX / 2);
+            float tempStartPosZ = gridArray[x, 0].cellPos.z + (cellSizeZ / 2);
+            Vector3 tempStartPos = new Vector3(tempStartPosX, 0, tempStartPosZ);
+
+            float tempEndPosX = gridArray[x, 0].cellPos.x + (cellSizeX / 2);
+            float tempEndPosZ = gridArray[x, 0].cellPos.z + ((cellSizeZ * gridLength));
+            Vector3 tempEndingPos = new Vector3(tempEndPosX, 0, tempEndPosZ);
+
+            Gizmos.DrawLine(tempStartPos, tempEndingPos);
+            
         }
     }
 
